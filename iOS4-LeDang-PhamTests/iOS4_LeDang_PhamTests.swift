a5a89f08641c29d64b13b89cd77837e8b817c10b
Kim Ngan <kimngan.ledang@gmail.com>
2020-11-25 15:31:11 +0100

    Initial Commit

---
+++ iOS4-LeDang-PhamTests/iOS4_LeDang_PhamTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS4_LeDang_PhamTests.swift
+//  iOS4-LeDang-PhamTests
+//
+//  Created by Kim on 25.11.20.
+//
+
+import XCTest
+@testable import iOS4_LeDang_Pham
+
+class iOS4_LeDang_PhamTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
