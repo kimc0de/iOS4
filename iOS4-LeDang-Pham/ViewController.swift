232d54e8f30148c2d64e8d7a74c627253482dbff
Kim Ngan <kimngan.ledang@gmail.com>
2020-12-02 22:52:15 +0100

    Add IBOUtlet

--- iOS4-LeDang-Pham/ViewController.swift
+++ iOS4-LeDang-Pham/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var euro_dolar_Rate: UITextField!
+    @IBOutlet weak var euro_pound_Rate: UITextField!
+    @IBOutlet weak var euroValue: UITextField!
+    @IBOutlet weak var dolarValue: UITextField!
+    @IBOutlet weak var poundValue: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
