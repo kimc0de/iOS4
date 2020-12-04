bd582fa23202a4f8b90c0f7544d6aa6e8244852b
Ba Tung Linh Pham <tunglinhphamba@Tungs-MacBook-Pro.local>
2020-12-04 15:27:32 +0100

    .

--- iOS4-LeDang-Pham/ViewController.swift
+++ iOS4-LeDang-Pham/ViewController.swift
@@ -32,37 +32,41 @@ class ViewController: UIViewController {
     @IBAction func dollarRateChanged(_ sender: Any) {
         euroToDollarRate = Double(euroDollarRate.text ?? "") ?? 0.0
         dollar = euro * euroToDollarRate
-        dollarValue.text = String(format: "%f", dollar)
+        dollarValue.text = doubleToString(doubleValue: dollar)
     }
     
     @IBAction func poundRateChanged(_ sender: Any) {
         euroToPoundRate = Double(euroPoundRate.text ?? "") ?? 0.0
         pound = euro * euroToPoundRate
-        poundValue.text = String(format: "%f", pound)
+        poundValue.text = doubleToString(doubleValue: pound)
     }
     
     @IBAction func euroValueChanged(_ sender: Any) {
         euro = Double(euroValue.text ?? "") ?? 0.0
         dollar = euro * euroToDollarRate
         pound = euro * euroToPoundRate
-        dollarValue.text = String(format: "%f", dollar)
-        poundValue.text = String(format: "%f", pound)
+        dollarValue.text = doubleToString(doubleValue: dollar)
+        poundValue.text = doubleToString(doubleValue: pound)
     }
     
     @IBAction func dollarValueChanged(_ sender: Any) {
         dollar = Double(dollarValue.text ?? "") ?? 0.0
         euro = dollar / euroToDollarRate
         pound = euro * euroToPoundRate
-        euroValue.text = String(format: "%f", euro)
-        poundValue.text = String(format: "%f", pound)
+        euroValue.text = doubleToString(doubleValue: euro)
+        poundValue.text = doubleToString(doubleValue: pound)
     }
     
     @IBAction func poundValueChanged(_ sender: Any) {
         pound = Double(poundValue.text ?? "") ?? 0.0
         euro = pound / euroToPoundRate
         dollar = euro * euroToDollarRate
-        euroValue.text = String(format: "%f", euro)
-        dollarValue.text = String(format: "%f", dollar)
+        euroValue.text = doubleToString(doubleValue: euro)
+        dollarValue.text = doubleToString(doubleValue: dollar)
+    }
+    
+    func doubleToString(doubleValue: Double) -> String {
+        return String(format: "%.2lf", doubleValue)
     }
 }
 
